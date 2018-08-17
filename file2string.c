f94df471b42b112672398969f4781941cbf8cea9
ttay <ttay@umich.edu>
2018-08-17 14:51:42 +0800

    Now prints newline & others
    - in the line now represents STDIN.

--- file2string.c
+++ file2string.c
@@ -16,16 +16,20 @@ void fileparsecopy(FILE* ifp, FILE* ofp){
         else
             putc(c, ofp);
     }
+    /* End off with a newline */
+    putc('\n', ofp);
 }
 
 
 int main(int argc, char **argv) {
     FILE * fp;
     const char* prog = argv[0];
-    const char* helpStr = "Usage:\n    file2str [FILE] ...\n\nDescription:\n    Outputs a string that represents the file(s). If no FILE is named, read from STDIN\n";
-    const char* versionStr = "file2string version 1.0.0\nCopyright (C) 2018 Thomas Tay.\nLicence: GPLv3+; GPL version 3 or later\nThis is free software, you are free to change and distribute it\nThere is NO WARRANTY WHATSOEVER\n";
-    if(argc == 1) /* no args, copy std input */
+    const char* helpStr = "Usage: \n    file2str [FILE] ...\n\nDescription:\n    Outputs a string that represents the file(s), followed by a newline. \n    If no FILE is named, read from STDIN.\n    '-' represents STDIN.\n";
+    const char* versionStr = "file2string version 1.1.0\nCopyright (C) 2018 Thomas Tay.\nLicence: GPLv3+; GPL version 3 or later\nThis is free software, you are free to change and distribute it\nThere is NO WARRANTY WHATSOEVER\n";
+    if(argc == 1){ /* no args, copy std input */
         fileparsecopy(stdin, stdout);
+        return 0;
+    }
 
     if(strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0){
         printf("%s", helpStr);
@@ -38,9 +42,13 @@ int main(int argc, char **argv) {
 
     while(--argc > 0){
         if((fp = fopen(*++argv, "r")) == NULL){
+            if (strcmp(*argv, "-") == 0){
+                fileparsecopy(stdin, stdout);
+            } else {
             fprintf(stderr, "%s: Can't open %s\n", prog, *argv);
             return 1;
-        } else{
+            }
+        } else {
             fileparsecopy(fp, stdout);
             fclose(fp);
         }
